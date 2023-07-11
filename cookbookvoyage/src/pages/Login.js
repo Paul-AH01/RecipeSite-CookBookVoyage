4528cce8577fe98d21487431c80262c6e091244f
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-07-11 16:26:52 +0100

    Today's update

--- cookbookvoyage/src/pages/Login.js
+++ cookbookvoyage/src/pages/Login.js
@@ -1,47 +1,22 @@
-// import React from "react";
-// import "./App.css";
-// import NavBar from "./Components/Navbar.js";
-// import cake from "./images/cake.png";
+import "./App.css";
+import React, {useState} from "react";
 
+//export const login = {props} => {
+//const [username, setUsername] = useState("")
+  //const [pass,setPass] = useState("");
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className="top-background"> 
-//         <h1 className="top-background-text">Cook Book Voyage</h1>
-//       </div>
-      
-//       {/* below is the code for the navbar using the ul and li tags */}
-//       <div className="navbar">
-//         <ul>
-//           <li>
-//             <a href="#home">Home</a>
-//           </li>
-//           <li>
-//             <a href="#profile">Profile</a>
-//           </li>
-//           <li>
-//             <a href="#recipes">Recipes</a>
-//           </li>
-//           <li>
-//             <a href="#popular">Popular</a>
-//           </li>
-//         </ul>
-//       {/* below is the code for the search bar using the input and button tags  */}
-//         <div className="search-bar">
-//           <input type="text" placeholder="Search..." />
-//           <button type="submit">Go</button>
-//         </div>
-//       </div>
-
-
-//       <form onSubmit={handleSubmit}> 
-//         <label for="username">Username</label>
-//<input type="username" placeholder="username" id="username" name="Username">
-//<label for="email">E-mail</label>
-//         <input value={email} onChange={(e) => setEmail(e.target.value)type="email" placeholder="youremail@mail.com" id="email" name="email">
-//             <label for="Password">Password</label>
-//             <input value={pass} onChange={(e) => setPass(e.target.value)} type="Password" placeholder="*******" id="password" name="password">
-//<button type="submit">Login</button> </form>
-//<Button>Dont have an account yet? Register here </button>
-//
+ // const handleSubmit = {e} => {
+  //  e.preventDefault();
+  //  console.log(username);
+ // }
+//return {
+<div className="loginForm">
+ <form onSubmit={handleSubmit}
+        <label for="username">Username </>
+<input type="username" placeholder="username" id="username" name="Username">
+            <label for="Password">Password :</label>
+        <input value={pass} onChange={(e) => setPass(e.target.value)} type="Password" placeholder="*******" id="password" name="password">
+<button type="submit">Sign in</button>
+<button>No account yet? Register here </button>
+</div>
+}
