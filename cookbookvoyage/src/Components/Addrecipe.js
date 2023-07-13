05445c7149ba571f6f79a423f9af638660478aca
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-07-13 17:14:08 +0100

    13th of July

--- cookbookvoyage/src/Components/Addrecipe.js
+++ cookbookvoyage/src/Components/Addrecipe.js
@@ -1,17 +1,38 @@
 function addRecipes (){
-return (
+
+  const [recipe,setRecipe] = useState("");
+  const [serving,setServing] = useState("");
+  const [time,setTime] = useState("");
+  const [calories,setCalories] = useState("");
+  const [ingredients,setIngredients] = useState("");
+  const [method,setMethod] = useState("");
+    
+  return (
 <div className="addRecipe">
-//RecipeName
-<label htmlFor="Time"> Time to make</label>
-<label htmlFor="Servings"> Number Of Servings</label>
-<label htmlFor="Time"> Time to make</label>
-<label htmlFor="Time"> Time to make</label>
-<label htmlFor="Time"> Time to make</label>
-<label htmlFor="Calories"> Calories/ Serving</label>
-<label htmlFor="Ingredients"> Ingredients</label>
-<label htmlFor="Method"> Method</label>
+
+<label htmlFor="Name">Recipe Name</label>
+<input type="name" placeholder="name" id="name" name="Recipe Name"/>
+
+<label htmlFor="Servings">Number Of Servings</label>
+<input type="text" placeholder="servings" id="servings" name="servings"/>
+
+<label htmlFor="Time">Time to make</label>
+<input type="Time" placeholder="time" id="time" name="Time to Make"/>
+
+<label htmlFor="Calories">Calories/ Serving</label>
+<input type="text" placeholder="Calories" id="Calories" name="Calories"/>
+
+<label htmlFor="Ingredients">Ingredients</label>
+<input type="text" placeholder="ingredients" id="ingredients" name="Ingredients"/>
+
+<label htmlFor="Method">Method</label>
+<input type="text" placeholder="Method" id="method" name="Method"/>
+
 <label htmlFor="Uploadimage">Upload Image</label>
+<input type="image" placeholder="image" id="image" name="image"/>
+
 <button type="submit">Upload Image</button>
+
 </div>
 )
 }
