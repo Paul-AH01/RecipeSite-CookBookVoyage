72a4831b0ae33914ac189fe6dab11bd332305867
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-07-13 09:41:41 +0100

    13th

---
+++ cookbookvoyage/src/Components/Addrecipe.js
@@ -0,0 +1,17 @@
+function addRecipes (){
+return (
+<div className="addRecipe">
+//RecipeName
+<label htmlFor="Time"> Time to make</label>
+<label htmlFor="Servings"> Number Of Servings</label>
+<label htmlFor="Time"> Time to make</label>
+<label htmlFor="Time"> Time to make</label>
+<label htmlFor="Time"> Time to make</label>
+<label htmlFor="Calories"> Calories/ Serving</label>
+<label htmlFor="Ingredients"> Ingredients</label>
+<label htmlFor="Method"> Method</label>
+<label htmlFor="Uploadimage">Upload Image</label>
+<button type="submit">Upload Image</button>
+</div>
+)
+}
