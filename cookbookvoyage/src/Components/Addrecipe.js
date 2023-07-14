04452f89f64bdca00837fcf4312b0d259c19dd49
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-07-14 14:49:38 +0100

    logreg update

--- cookbookvoyage/src/Components/Addrecipe.js
+++ cookbookvoyage/src/Components/Addrecipe.js
@@ -1,38 +1,39 @@
-function addRecipes (){
+// function addRecipes (){
 
-  const [recipe,setRecipe] = useState("");
-  const [serving,setServing] = useState("");
-  const [time,setTime] = useState("");
-  const [calories,setCalories] = useState("");
-  const [ingredients,setIngredients] = useState("");
-  const [method,setMethod] = useState("");
+//   const [recipe,setRecipe] = useState("");
+//   const [serving,setServing] = useState("");
+//   const [time,setTime] = useState("");
+//   const [calories,setCalories] = useState("");
+//   const [ingredients,setIngredients] = useState("");
+//   const [method,setMethod] = useState("");
     
-  return (
-<div className="addRecipe">
+//   return (
+    
+// <div className="addRecipe">
 
-<label htmlFor="Name">Recipe Name</label>
-<input type="name" placeholder="name" id="name" name="Recipe Name"/>
+// <label htmlFor="Name">Recipe Name</label>
+// <input type="name" placeholder="name" id="name" name="Recipe Name"/>
 
-<label htmlFor="Servings">Number Of Servings</label>
-<input type="text" placeholder="servings" id="servings" name="servings"/>
+// <label htmlFor="Servings">Number Of Servings</label>
+// <input type="text" placeholder="servings" id="servings" name="servings"/>
 
-<label htmlFor="Time">Time to make</label>
-<input type="Time" placeholder="time" id="time" name="Time to Make"/>
+// <label htmlFor="Time">Time to make</label>
+// <input type="Time" placeholder="time" id="time" name="Time to Make"/>
 
-<label htmlFor="Calories">Calories/ Serving</label>
-<input type="text" placeholder="Calories" id="Calories" name="Calories"/>
+// <label htmlFor="Calories">Calories/ Serving</label>
+// <input type="text" placeholder="Calories" id="Calories" name="Calories"/>
 
-<label htmlFor="Ingredients">Ingredients</label>
-<input type="text" placeholder="ingredients" id="ingredients" name="Ingredients"/>
+// <label htmlFor="Ingredients">Ingredients</label>
+// <input type="text" placeholder="ingredients" id="ingredients" name="Ingredients"/>
 
-<label htmlFor="Method">Method</label>
-<input type="text" placeholder="Method" id="method" name="Method"/>
+// <label htmlFor="Method">Method</label>
+// <input type="text" placeholder="Method" id="method" name="Method"/>
 
-<label htmlFor="Uploadimage">Upload Image</label>
-<input type="image" placeholder="image" id="image" name="image"/>
+// <label htmlFor="Uploadimage">Upload Image</label>
+// <input type="image" placeholder="image" id="image" name="image"/>
 
-<button type="submit">Upload Image</button>
+// <button type="submit">Upload Image</button>
 
-</div>
-)
-}
+// </div>
+// )
+// }
