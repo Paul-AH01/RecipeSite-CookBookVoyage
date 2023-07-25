73936c496c1e0a68df6ca532041c1384a7a08e5a
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-07-25 09:47:03 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin:
+}));
 
 app.use('/authenticate', userRouter);
 app.use('/recipes', recipesRouter);
