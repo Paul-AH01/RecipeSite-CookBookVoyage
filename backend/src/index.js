31aaa9e69116b7de335708d2226f7c23f53caa09
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-07-26 16:00:26 +0100

    fix pls

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://cookbookvoyage.onrender.com"
+  origin: ["https://cookbookvoyage.onrender.com", "http://localhost:3000"]
 }));
 
 
