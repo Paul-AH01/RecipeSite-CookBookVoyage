de5ca887d1a15b163be7f753544878dc403c1fdd
JohnRatcliffe <jibletjr32@gmail.com>
2023-07-27 14:17:46 +0100

    Pls pls work

--- cookbookvoyage/src/pages/Home.js
+++ cookbookvoyage/src/pages/Home.js
@@ -16,7 +16,7 @@ export const Home = () => {
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await axios.get("https://cookbookvoyage.onrender.com");
+        const response = await axios.get("http://localhost:3001/recipes");
         setRecipes(response.data);
       } catch (err) {
         console.error(err);
--- cookbookvoyage/src/pages/add-recipes.js
+++ cookbookvoyage/src/pages/add-recipes.js
@@ -38,10 +38,10 @@ export const AddRecipes = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-        const data = await  axios.post("https://cookbookvoyage.onrender.com/add-recipes", recipes);
+        const data = await  axios.post("http://localhost:3001/recipes", recipes);
         console.log(data);
         alert("Recipe added!");
-        // navigate("/");
+        navigate("/");
     } catch (err) {
         console.error(err);
     }        
